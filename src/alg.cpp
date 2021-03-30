32384d87e1d4b4984d056f7e5ae12e5d581b2565
OwlInside <78905764+OwlInside@users.noreply.github.com>
2021-03-30 21:41:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,23 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+        int l = 0, r = size-1;
+        int count = 0;
+        while (l < r){
+                int midd = (l + r) / 2;
+                if(arr[midd] < value)
+                        l = midd+1;
+                else
+                        r = midd;
+                if (arr[l] == value) {
+                        while (arr[l] == value) {
+                                count++;
+                                l++;
+                        }
+                }
+        }
+        if (count != 0)
+                return count;
+        else
+                return 0; // если ничего не найдено
 }
